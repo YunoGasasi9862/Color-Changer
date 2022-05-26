719e263d41dcb9849f2e686acff73a8bbb17ff6b
Muhammad Bilal <m.bilal9862@gmail.com>
2022-05-26 12:49:44 +0300

    followplayer

--- Assets/FollowPlayer.cs
+++ Assets/FollowPlayer.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Transform player;
     void Start()
     {
-        
+        player = GameObject.FindWithTag("Player").GetComponent<Transform>();    
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+         if(transform.position.y < player.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, player.position.y, transform.position.z);  //camera follow
+        }
     }
 }
