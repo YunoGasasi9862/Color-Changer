207a5d9a5987a7a4035d4b955eea929e926d78e3
Muhammad Bilal <m.bilal9862@gmail.com>
2022-05-30 13:06:18 +0300

    script

--- Assets/LevelGenerator.cs
+++ Assets/LevelGenerator.cs
@@ -4,19 +4,24 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    public GameObject Rottor;
-    float maxSpeed;
-    float minSpeed;
+    public GameObject Rotator;
+    float maxSpeed=500;
+    float minSpeed=200;
     int maxNumberofRotators = 10;
-    float miny;
-    float maxy;
-    void Start()
+    float Height=2;
+ 
+    void Start() //at the very start of the level 
     {
+        Vector2 position = transform.position;
 
         for(int i=0; i<maxNumberofRotators; i++)
         {
 
-
+            float newSpeed = Random.Range(minSpeed, maxSpeed);
+            Rotator.GetComponent<Rotator>().speed = newSpeed; //this sets the new speed for the rotator
+            position.y = Height; //at every 15
+            Instantiate(Rotator, position, Quaternion.identity);
+            Height += 10; //increment
 
         }
     }
