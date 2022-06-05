162c93fe901598bc480139cb5c7711cf28746f1b
Muhammad Bilal <m.bilal9862@gmail.com>
2022-06-05 12:54:53 +0300

    o

--- Assets/LevelGenerator.cs
+++ Assets/LevelGenerator.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class LevelGenerator : MonoBehaviour
 {
     public GameObject Rotator;
     float maxSpeed=500;
     float minSpeed=200;
-    int maxNumberofRotators = 10;
+    int maxNumberofRotators;
     float Height=2;
+
+    void CheckLevel()
+    {
+        if(SceneManager.GetActiveScene().name=="Level1")
+        {
+            maxNumberofRotators = 10;
+
+        }else if(SceneManager.GetActiveScene().name=="Level2")
+        {
+
+            maxNumberofRotators = 15;
+        }else if(SceneManager.GetActiveScene().name=="Level3")
+        {
+            maxNumberofRotators = 20;
+        }
+
+    }
  
     void Start() //at the very start of the level 
     {
         Vector2 position = transform.position;
+        CheckLevel();
 
         for(int i=0; i<maxNumberofRotators; i++)
         {
