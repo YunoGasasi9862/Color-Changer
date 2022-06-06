1a50eba3943fe6b17aec30b94112704b67ca6553
Muhammad Bilal <m.bilal9862@gmail.com>
2022-06-06 12:50:37 +0300

    ss

--- Assets/ColorChangerGenerator.cs
+++ Assets/ColorChangerGenerator.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class ColorChangerGenerator : MonoBehaviour
 {
     float maxheight = 10;
     public GameObject colorChanger;
-    int numberofObjects = 9;
+    int numberofObjects;
+
+    void GeneratorBalls()
+    {
+        if(SceneManager.GetActiveScene().name=="Level1")
+        {
+
+            numberofObjects = 9;
+        }else if(SceneManager.GetActiveScene().name=="Level2")
+        {
+
+
+            numberofObjects = 14;
+        }else if(SceneManager.GetActiveScene().name=="Level3")
+        {
+
+            numberofObjects = 19;
+        }
+
+
+    }
     void Start()
     {
         Vector2 position = transform.position;
-        for(int i=0; i < numberofObjects; i++)
+        GeneratorBalls();
+        for (int i=0; i < numberofObjects; i++)
         {
-            position.y += maxheight;
+            position.y += maxheight;  //place them at equal heights
             Instantiate(colorChanger, position, Quaternion.identity);
 
         }
